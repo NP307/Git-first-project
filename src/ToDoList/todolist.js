5acbee3d8c9cfacba6a8662b719e1d4539fa80de
NP307 <40033104+np307@users.noreply.github.com>
2022-03-09 12:20:40 +0200

    clear code

--- src/ToDoList/todolist.js
+++ src/ToDoList/todolist.js
@@ -31,37 +31,7 @@ class ToDoList extends React.Component {
         }
     }
 
-    /*componentDidMount() {
-        setInterval(() => {
-            console.log(this.state, this.props);
-        }, 2000)
-    }*/
-
     handleClick(index) {
-        /*this.setState((state, props) => {
-            const array = state.ToDoArray.map((item, innerIndex) => {
-                if(outerIndex === innerIndex) {
-                    item.isChecked = !item.isChecked;
-                }
-                console.log(item, props);
-                return item
-            })
-
-
-            return {
-                ToDoArray: array
-            }
-        });*/
-
-        /*let key = 2;
-        this.setState(prevState => ({
-
-            ToDoArray: prevState.ToDoArray.map(
-                el => el.key === key? { ...el, isChecked: true }: el
-            )
-
-        }));*/
-
         this.setState((state) => {
             const array = state.ToDoArray;
             array[index] = {
@@ -74,27 +44,9 @@ class ToDoList extends React.Component {
             }
         });
 
-        /*this.setState((state) => {
-            let array = state.isLol;
-            array = !array;
-            return {
-                isLol: array
-            }
-        });*/
-        console.log(this.state);
-
     };
 
     render() {
-        /*let ToDoList = this.ToDoArray.map((list,index) => {
-            return <li onClick={() => {this.ToDoArray[index].isChecked = !this.ToDoArray[index].isChecked}} className={list.isChecked ? "fin" : ""} key={index}>{list.number}</li>
-        });
-        return (
-            <ul>{ToDoList}</ul>
-        )*/
-
-
-
         return (
             <div>
                 <p className={this.state.ToDoArray[0].isChecked ? "fin" : ""} onClick={() => this.handleClick(0)}>{this.state.ToDoArray[0].number}</p>
