014bee0af1143db0c704fd4104500f09a6de5876
NP307 <40033104+np307@users.noreply.github.com>
2022-03-10 18:22:30 +0200

    routing + create new list component that gets list from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import Wrapper from './Wrapper/Wrapper'
+import Wrapper from './Wrapper/Wrapper';
+import Logo from './Logo';
+import { Routes, Route } from "react-router-dom";
+import ToDoRequest from "./ToDoRequest/ToDoList";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Wrapper />
+        <Routes>
+          <Route path='/' element={<Logo />} />
+          <Route path='/todolist' element={<Wrapper />} />
+        </Routes>
+        <ToDoRequest />
       </header>
     </div>
   );
